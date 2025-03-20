1a57f15895e23896b80a35aec6ccba9befc1117d
Steven Wacks <steven.wacks@matrixspace.com>
2025-03-20 15:21:59 -0400

    highlights to indents

--- lua/visual.lua
+++ lua/visual.lua
@@ -21,4 +21,29 @@ vim.cmd("hi MatchParen ctermbg=240")
 
 ------------------------------------------------------------------------------------------
 
-require("ibl").setup()
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#7A3A41" })  -- #E06C75
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#8F713E" })  -- #E5C07B
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#315F87" })  -- #61AFEF
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#805A3A" })  -- #D19A66
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#4F6E49" })  -- #98C379
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#713F8A" })  -- #C678DD
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#2E646C" })  -- #56B6C2
+end)
+
+require("ibl").setup { indent = { highlight = highlight } }
+
+--require("ibl").setup()
