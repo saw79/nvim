f30ba48e08b464e28645e1ad72dcb4d51d5928ce
Steven Wacks <steven.wacks@matrixspace.com>
2025-04-01 12:20:17 -0400

    turned off lazy checker notify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -168,7 +168,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
   -- consider installing luarocks
   rocks = { enabled = false },
 })
